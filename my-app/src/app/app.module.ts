9a2998e998ef5ce48cfde0bd0b143003cbeab2fe
Suraj Singh <surajs9763@gmail.com>
2018-03-03 14:30:51 +0530

    Added surajcomponent

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SurajComponent } from './suraj/suraj.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SurajComponent
   ],
   imports: [
     BrowserModule,
